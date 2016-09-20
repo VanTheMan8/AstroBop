9bde38e7be77994200ae6e109b9fdcd2d007ddff
VanTran <van.tran080575@gmai.com>
2016-09-20 19:53:09 -0300

    Initial Commit

---
+++ AstroBop Generate/Assets/Scripts/RandomMaterialColor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class RandomMaterialColor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Renderer r = GetComponent<Renderer>();
+		r.material.color = new Color(
+			0.8f + Random.value * 0.8f, 
+			0.8f + Random.value * 0.8f, 
+			0.8f + Random.value * 0.8f 
+		);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
